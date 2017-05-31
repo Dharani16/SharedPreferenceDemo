96f7fb84225ac5d9632712f319cef1771cf13aa2
Dharani16 <rdharani1606@gmail.com>
2017-05-31 16:57:31 +0530

    sharedpreference

--- app/src/main/java/com/dharani/sharedpreferencedemo/MainActivity.java
+++ app/src/main/java/com/dharani/sharedpreferencedemo/MainActivity.java
@@ -2,9 +2,11 @@ package com.dharani.sharedpreferencedemo;
 
 import android.content.Context;
 import android.content.SharedPreferences;
+import android.preference.PreferenceManager;
 import android.support.design.widget.TextInputLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -40,39 +42,43 @@ public class MainActivity extends AppCompatActivity {
         btSave = (Button) findViewById(R.id.button_Save);
         btDisplay = (Button) findViewById(R.id.button_Display);
 
-        stName = etName.getText().toString();
-        stContact = etContact.getText().toString();
-        stEmail = etEmail.getText().toString();
-        stLocation = etLocation.getText().toString();
-
         sharedPreferences = getSharedPreferences("Mypreferences", Context.MODE_PRIVATE);
 
         btSave.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                stName = etName.getText().toString();
+                stContact = etContact.getText().toString();
+                stEmail = etEmail.getText().toString();
+                stLocation = etLocation.getText().toString();
+
                 SharedPreferences.Editor editor = sharedPreferences.edit();
                 editor.putString("Name",stName);
                 editor.putString("Contact",stContact);
                 editor.putString("Email",stEmail);
                 editor.putString("Location",stLocation);
-                if(editor.commit())
-                {
-                Toast.makeText(MainActivity.this, "Saved", Toast.LENGTH_SHORT).show();
-//                    editor.clear();
-//                    editor.commit();
-            }}
+                editor.commit();
+
+                if (editor.commit()){
+                    Toast.makeText(MainActivity.this, "saved", Toast.LENGTH_SHORT).show();
+                }
+                else {
+                    Toast.makeText(MainActivity.this, "not save", Toast.LENGTH_SHORT).show();
+                }
+
+            }
         });
 
         btDisplay.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-              String  n = sharedPreferences.getString("Name","gopinath");
+                String  n = sharedPreferences.getString("Name",null);
                 String c = sharedPreferences.getString("Contact",null);
                 String e = sharedPreferences.getString("Email",null);
                 String l = sharedPreferences.getString("Location",null);
 
-                Toast.makeText(MainActivity.this, "Name is "+n, Toast.LENGTH_SHORT).show();
-                tvResult.setText(n +"\n"+ c + "\n"+ e + "\n"+l);
+               // Toast.makeText(MainActivity.this, "Name is "+n, Toast.LENGTH_SHORT).show();
+                tvResult.setText("Name : "+n + "\n" + "Contact : "+c +"\n"+ "Email : "+e +"\n"+ "Location :"+l);
 
             }
         });
